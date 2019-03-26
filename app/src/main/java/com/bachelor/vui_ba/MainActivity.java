05d472a795209456d7a33569bd42e94502c3f01b
leameierx <meier.lea@icloud.com>
2019-03-26 14:11:40 +0100

    integrated Nuance

--- app/src/main/java/com/bachelor/vui_ba/MainActivity.java
+++ app/src/main/java/com/bachelor/vui_ba/MainActivity.java
@@ -2,12 +2,108 @@ package com.bachelor.vui_ba;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import com.nuance.speechanywhere.Session;
+import com.nuance.speechanywhere.SessionEventListener;
+import com.nuance.speechanywhere.VuiController;
+import com.nuance.speechanywhere.VuiControllerEventListener;
+
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity implements SessionEventListener, VuiControllerEventListener {
+    private VuiController theVuiController; // Reference to the VuiController object
+    private EditText spokenText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+        registerOnNuance();
+        configNuance();
+        Session.getSharedSession().startRecording();
+    }
+
+    private void init(){
+        spokenText = findViewById(R.id.spokenText);
+        theVuiController = findViewById(R.id.vuicontroller);
+    }
+
+    private void configNuance(){
+        theVuiController.setTopic("GeneralMedicine");
+        theVuiController.setLanguage("de");
+    }
+
+    private void registerOnNuance(){
+        Registration r = new Registration();
+        r.openNuanceSession();
+    }
+
+    // Save recording state across destruction-recreation (for example, on device rotation)
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    // Activity lifecycle callbacks to register and unregister for session event callbacks properly
+    @Override
+    protected void onStart() {
+        // When the Activity starts (it is already created), register for session event callbacks
+        Session.getSharedSession().addSessionEventListener(this);
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        // When the Activity stops, unregister for session event callbacks
+        Session.getSharedSession().removeSessionEventListener(this);
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        // When the Activity is about to be destroyed, close session.
+        // From the application logic we know that speech recognition is not needed beyond this point.
+        if (isFinishing()) Session.getSharedSession().close();
+        super.onDestroy();
+    }
+
+
+        @Override
+    public void onRecordingStarted() {
+
+    }
+
+    @Override
+    public void onRecordingStopped() {
+
+    }
+
+    @Override
+    public void onProcessingStarted() {
+
+    }
+
+    @Override
+    public void onProcessingFinished() {
+
+    }
+
+    @Override
+    public void onProcessingStarted(View view) {
+
+    }
+
+    @Override
+    public void onProcessingFinished(View view) {
+
+    }
+
+    @Override
+    public void onCommandRecognized(String s, String s1, String s2, HashMap<String, String> hashMap) {
+
     }
 }
--- app/src/main/java/com/bachelor/vui_ba/Registration.java
+++ app/src/main/java/com/bachelor/vui_ba/Registration.java
@@ -1,4 +1,26 @@
 package com.bachelor.vui_ba;
 
+import com.nuance.speechanywhere.Session;
+
 public class Registration {
+    // Convenience defines used for opening the Session instance and pass it the licensing
+    // information needed. Your partner GUID and organization token will be made available to you via the
+    // Nuance order desk.
+    private static final String applicationName = "Android Sample";
+
+    // THIS IS CUSTOMER SPECIFIC - MUST *NOT* BE HARD-CODED!
+    // THIS IS EQUIVALENT TO A LICENSE KEY - MUST BE KEPT SECRET FROM UNAUTHORIZED USERS!
+    // Make it configurable via user settings or download it from your server if you have a client-server app.
+    private String organizationToken = "B7ECC65F-1790-42A9-89FF-606D72DA57ED";
+
+    // It is ok to hard-code the partner GUID - should be "hidden" within your application,
+    // usually will not need to be changed.
+    private String partnerToken = "a76110e5-2bad-4b54-9abf-8a3b15619e66";
+
+    private static final String userId = "bachelorthesis";
+
+    public void openNuanceSession() {
+        // Open the session by supplying the given user name, the proper credentials and the application's name
+        Session.getSharedSession().open(userId, organizationToken, partnerToken, applicationName);
+    }
 }
