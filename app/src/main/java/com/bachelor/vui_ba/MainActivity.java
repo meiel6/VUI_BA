53f40495af3a863464a8fa82bcf6c08a46dc6591
leameierx <meier.lea@icloud.com>
2019-04-17 11:13:57 +0200

    added transparent logo as background

--- app/src/main/java/com/bachelor/vui_ba/MainActivity.java
+++ app/src/main/java/com/bachelor/vui_ba/MainActivity.java
@@ -4,13 +4,18 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.BatteryManager;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.nuance.speechanywhere.CommandSet;
